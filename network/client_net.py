1e5d9bb88a4540d4d298264c35af3e4400929c53
Chirag Wadehra <s4014390@student.rmit.edu.au>
2025-06-24 19:48:49 +1000

    ClientSide: Added methods for Connecting to serverSocket AND SendingFile to the server Socket

---
+++ network/client_net.py
@@ -0,0 +1,35 @@
+# Establish a TCP connection among the client and server.
+# This file will handle the client side connection, so connecting to the server using socket's IP and port number
+
+import socket #
+import os
+
+class client_net:
+    def __init__(self, serverIP, serverPort):
+        self.serverIP = serverIP
+        self.serverPort = serverPort
+        self.serverSocketAddress = (serverIP, serverPort)
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Af_inet is for ipv4 addresses and Sock_stream is for TCP connections
+    
+    def connectToServerSocket(self):
+        # connect to the server socket through .connect()
+        self.clientSocket.connect(self.serverSocketAddress)
+    
+    def sendingFilesToServer(self, filePath):
+
+        #sending the file name
+        fileName = os.path.basename(filePath)
+        self.clientSocket.sendall((fileName + '\n').encode())
+    
+        #sending the file size
+        fileSize = os.path.getsize(filePath)
+        self.clientSocket.sendall((str(fileSize) + '\n').encode())
+
+        #loop that reads the file in chunks and sends one chunk at a time
+        chunksSize = 1024
+        with open(filePath, 'rb') as file:
+            while True:
+                data = file.read(chunksSize)
+                if not data:
+                    break
+                self.clientSocket.sendall(data)
